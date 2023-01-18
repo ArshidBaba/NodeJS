d8b1645256d234894d862ff4204008f1294ab6e9
Arshid <arshidbaba93@gmail.com>
2023-01-18 15:03:01 +0530

    Learned basic CRUD in node

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/courses/', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 })
 
 app.get('/api/courses/:id', (req, res) => {
@@ -24,14 +25,62 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses/', (req, res) => {
-    console.log(req.body)
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    const result = schema.validate({ name: req.body.name });
+    // const result = Joi.va
+    console.log(result);
+    
+    if (result.error) {
+        res.status(400).send(result.error.message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
+    console.log(courses);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given Id does not exist');
+
+    const { error } = validateCourse(req.body);
+
+     
+    if (error) {
+        res.status(400).send(error.message);
+        return;
+    }
+
+    course.name = req.body.name;
+    console.log(courses);
+    res.send(course);
+});
+
+app.delete('/api/courses/:id/', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given Id does not exist');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate({ name: course.name });
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
